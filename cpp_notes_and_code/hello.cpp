3fd2e4477bb08c9563b3b1bdab8af111022a60dc
kristinalagasca <kristinalagasca@gmail.com>
2021-11-02 20:47:02 -0400

    adding some code

---
+++ cpp_notes_and_code/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "You are a civilian who just woke up from a coma...";
+    std::cout << std::endl;
+    std::cout << "Your goal of this game is to survive...";
+
+    return 0;
+}
