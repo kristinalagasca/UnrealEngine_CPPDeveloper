7f8e8bfb67b4f5b86224d43a6b3fbe51da164693
kristinalagasca <kristinalagasca@gmail.com>
2021-11-04 14:12:34 -0400

    hello world

--- cpp_notes_and_code/hello.cpp
+++ cpp_notes_and_code/hello.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
+/*
+The code below is an example of bad naming convention when it comes to Unreal Engine's coding standards
+*/
+
+
 int main() {
-    std::cout << "You are a civilian who just woke up from a coma...";
+    std::cout << "Your goal in this game is to guess 3 numbers...";
     std::cout << std::endl;
-    std::cout << "Your goal of this game is to survive...";
+    std::cout << "You have 3 attempts to guess the correct sequence...";
+    std::cout << std::endl;
+    std::cout << "If you do not guess them correctly within those 3 tries, you die...";
+    std::cout << std::endl;
+
+    // Declare 3 variables and assign to integers
+    int a = 4;
+    int b = 6;
+    int c = 7;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    // Print sum and product of those variables
+    std::cout << "The numbers add up to: " << sum << std::endl;
+    std::cout << "The numbers multiplied together will equal: " << product << std::endl;
 
     return 0;
 }
